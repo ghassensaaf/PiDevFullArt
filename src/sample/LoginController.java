6455580b98a77881fce8c8aaa22abc0c9d332a6f
salma <47942979+salmamouelhi@users.noreply.github.com>
2021-03-01 17:03:03 +0100

    badalt asemi

---
+++ src/sample/LoginController.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+
+public class LoginController {
+
+    @FXML
+    private Button btnlogin;
+
+    @FXML
+    private TextField txtlogin;
+
+    @FXML
+    private PasswordField pwd;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
