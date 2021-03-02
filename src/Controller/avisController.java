18238fe9cb84c90c1a2807d592a18854229aa27f
salma <47942979+salmamouelhi@users.noreply.github.com>
2021-03-02 11:07:50 +0100

    mm

--- src/Controller/avisController.java
+++ src/Controller/avisController.java
@@ -69,6 +69,6 @@ public class avisController implements Initializable {
     }
 
 
-    public ObservableList<avis>get
+
 
 }
--- src/home/Main.java
+++ src/home/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/avis.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/login.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
